48683fc9a792e950d9f9fa1ee6588269dd3db01e
Ryan Back <ho0405@gmail.com>
2023-09-26 10:14:51 -0600

    Assignment2

---
+++ components/StudentInfo.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <p>Ryan Back</p>
+      <p>CPRG 306 </p>
+      <p>
+        <Link href="https://github.com/ho0405">GitHub Repository</Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
